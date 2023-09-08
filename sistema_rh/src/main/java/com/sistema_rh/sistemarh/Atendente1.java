37d91cf18523a86d194681d04771ea94142781e2
RogerioLuis <139816265+RogerioLuis@users.noreply.github.com>
2023-09-07 23:06:40 -0300

    Third

---
+++ sistema_rh/src/main/java/com/sistema_rh/sistemarh/Atendente1.java
@@ -0,0 +1,24 @@
+package com.sistema_rh.sistemarh;
+
+public class Atendente1 extends Funcionario1 {
+
+    
+    public Atendente1(String nome, int idade, String endereco, String telefone, String email, int matricula, double salarioBase) {
+        super(nome, idade, endereco, telefone, email, matricula, salarioBase);
+    }
+
+   
+    @Override
+    public double calcularFGTS() {
+       
+        double salarioBase = getSalarioBase();
+        return salarioBase * 0.08; 
+    }
+
+    @Override
+    public double calcularDecimoTerceiro() {
+        
+        double salarioBase = getSalarioBase();
+        return salarioBase / 12; 
+    }
+}
