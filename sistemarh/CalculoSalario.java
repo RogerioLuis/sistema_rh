ee95c3b846cad7437cbc1e5ba505be6c508efd24
RogerioLuis <139816265+RogerioLuis@users.noreply.github.com>
2023-09-07 23:34:42 -0300

    This is the way

---
+++ sistemarh/CalculoSalario.java
@@ -0,0 +1,9 @@
+package com.sistema_rh.sistemarh;
+
+public interface CalculoSalario {
+    double calcularFGTS();
+    double calcularINSS();
+    double calcularDecimoTerceiro();
+    double calcularFerias();
+
+}
